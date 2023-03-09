5eb00547ada61590373d62d6cbb516ef63ad3e28
wolfsup7 <laurendooley0324@gmail.com>
2023-03-09 18:38:05 -0500

    add ability to show moves

--- src/Game.js
+++ src/Game.js
@@ -3,20 +3,20 @@ import Board from './Board';
 
 const calculateWinner = (squares) => {
   const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-          return squares[a];
-      }
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
   return null;
 }
@@ -51,6 +51,14 @@ class Game extends React.Component {
     }else{
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Move #' + move : 'Game start';
+      return (
+        <li>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
 
     return (
       <div className="game">
@@ -59,7 +67,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
         <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+        <ol>{ moves }</ol>
         </div>
       </div>
     );
