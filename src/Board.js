28cc3f3c8cbc62462427b7bb8fd3c6ad0373120e
wolfsup7 <laurendooley0324@gmail.com>
2023-03-09 18:02:12 -0500

    lift state up

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,20 @@ import React from 'react';
 import Square from './Square';
 
 class Board extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+    handleClick(i) {
+        // slice to copy squares
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares:squares});
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
     }
     render() {
         const status = 'Next player: X';
