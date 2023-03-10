5ed87aecff64c74b1c2440bbd0a6823504e1d473
wolfsup7 <laurendooley0324@gmail.com>
2023-03-09 21:00:09 -0500

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from '.logo.svg';
-import './App.css';
 import './TicTacToe.css';
-import Game from './Game.js';
+import Game from './Game';
 
 class App extends Component {
 
